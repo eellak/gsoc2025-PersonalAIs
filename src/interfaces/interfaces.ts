50bd666567fcf786c3133c8c05fe93850f321c3f
cocoshe <1228759711@qq.com>
2025-06-27 01:07:36 +0800

    enhence for fc

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -4,11 +4,30 @@ type Attachment = {
     url: string;
     };
 
+
+type ToolArgs = Record<string, any>;
+type ToolResultContent = any[];
+interface ToolResult {
+  content: ToolResultContent;
+}
+type ToolInvocationState = "pending" | "executing" | "result" | "error";
+
+interface ToolInvocation {
+  args: ToolArgs;
+  result?: ToolResult;
+  state: ToolInvocationState;
+  step: number;
+  toolCallId: string;
+  toolName: string;
+}
+
+    
   
 export interface message{
     content:string;
     role:string;
     id:string;
     experimental_attachments: Attachment[];
+    toolInvocations: ToolInvocation[];
 }
 
